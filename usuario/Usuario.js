861fbd1f2008300a05fdb5f6bd12fc9008cd2548
guirs51 <143645273+guirs51@users.noreply.github.com>
2024-08-14 21:29:37 -0300

    Add files via upload

---
+++ usuario/Usuario.js
@@ -0,0 +1,49 @@
+
+const Sequelize = require('sequelize');
+const database = require('../data_base/db');
+
+const Usuario = database.define('usuario', {
+  id: {
+    type: Sequelize.INTEGER,
+    autoIncrement: true,
+    allowNull: false,
+    primaryKey: true
+  },
+  nome: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+
+  email: {
+    type: Sequelize.STRING,
+    allowNull: true
+  },
+  senha: {
+    type: Sequelize.STRING,
+    allowNull: true
+  }
+}, {
+
+
+
+  // Configurações do modelo
+  timestamps: true, // Habilita createdAt e updatedAt
+  hooks: {
+    beforeCreate: (usuario, options) => {
+      const now = new Date();
+      const threeHoursLater = new Date(now.getTime() - 3 * 60 * 60 * 1000);
+      usuario.createdAt = threeHoursLater;
+      usuario.updatedAt = threeHoursLater;
+    },
+    beforeUpdate: (usuario, options) => {
+      const now = new Date();
+      const threeHoursLater = new Date(now.getTime() - 3 * 60 * 60 * 1000);
+      usuario.updatedAt = threeHoursLater;
+    }
+  }
+
+})
+
+
+
+module.exports = Usuario;
